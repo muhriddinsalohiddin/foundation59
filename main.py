fbe51b7f49e77d5ad3b29b3aab457e505e963fb4
muhriddinsalohiddin <troynms@gmail.com>
2022-08-17 21:23:05 +0500

    ABBOS UCHUN

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 """
 USHBU FILE DARS JARAYONIDA YOZILDI
 """
-print("SALOM")
-
-
 def Tubmi(son):
     if son < 2:
         return False
@@ -11,16 +8,3 @@ def Tubmi(son):
         if son % i == 0:
             return False
     return True
-
-
-for i in range(10):
-    print(i)
-
-"""
-Kamoliddin ko'p telefonga berilmang foydasi yo'q
-"""
-
-if int(input("SON KIRITING"))%2==0:
-    print("JUFT")
-else:
-    print("TOQ")
